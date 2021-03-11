ed5840399da49d9d0ec963638ee86f65e26725db
laurens <laurens.brosens@student.ap.be>
2021-03-11 15:07:21 +0100

    API added

---
+++ API/InternationalProjectASPCore/FloorballAPI/Controllers/MatchController.cs
@@ -0,0 +1,33 @@
+﻿using FloorballAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FloorballAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatchController : ControllerBase
+    {
+        private readonly MatchContext context;
+        public MatchController(MatchContext myContext)
+        {
+            context = myContext;
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public IActionResult GetPerson(int id)
+        {
+            var person = context.Players.Find(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return Ok(person);
+        }
+    }
+}
---
+++ API/InternationalProjectASPCore/FloorballAPI/Model/MatchContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FloorballAPI.Model
+{
+    public class MatchContext : DbContext
+    {
+        public MatchContext(DbContextOptions<MatchContext> options) : base(options)
+        {
+        }
+        public DbSet<Player> Players { get; set; }
+    }
+}
