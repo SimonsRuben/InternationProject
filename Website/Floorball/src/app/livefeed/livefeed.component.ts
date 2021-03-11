737dc070f85f1b15748ba4090f9ea89fe0d0734f
Simonsruben <ruben.simons01@student.ap.be>
2021-03-11 16:43:51 +0100

    design matchcontrol

---
+++ Website/Floorball/src/app/livefeed/livefeed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-livefeed',
+  templateUrl: './livefeed.component.html',
+  styleUrls: ['./livefeed.component.css']
+})
+export class LivefeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
