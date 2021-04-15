59b6cf4f893bb0f4fbcd2814038155a85c81358f
laurens <laurens.brosens@student.ap.be>
2021-04-15 14:15:46 +0200

    Select player for match

--- Website/Floorball/src/app/matchcontrol/matchcontrol.component.ts
+++ Website/Floorball/src/app/matchcontrol/matchcontrol.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiserviceService, Imatch, Iplayer, Iplayerinfo, Iteam,Iplayerstats } from '../Services/apiservice.service';
 
 @Component({
   selector: 'app-matchcontrol',
@@ -8,15 +9,43 @@ import { Component, OnInit } from '@angular/core';
 export class MatchcontrolComponent implements OnInit {
 
   resumetext: string ="Pause match";
-  constructor() { }
+  constructor(private api : ApiserviceService) { }
+  players : Iplayer[]
+  Teamlijst : Iteam[] = null
+  player : Iplayerinfo = null
 
   ngOnInit(): void {
+    this.api.Allteams.subscribe(d => {
+      this.Teamlijst = d;
+    });
+  }
+
+
+  getallplayers(event?: any)
+  {
+    var name = event.target.value;
+    if (name != null && name != "null") {
+      this.api.teaminfo(name).subscribe(d => {
+        this.players = d[0].players;
+      });
+    }
+
+  }
+  selectplayer(event? : any)
+  {
+    var name = event.target.value;
+    if (name != null && name != "null") {
+      this.api.playerinfo(name).subscribe(d => {
+        this.player = d[0];
+        this.api.getstats(name).subscribe();
+      });
+    }
   }
   StopMatch(){
 
   }
   CreateMatch(){
-    
+    this.api.creatematch(this.player.id).subscribe();
   }
 
   ResumePause()
